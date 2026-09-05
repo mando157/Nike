b0a5d2a8cf343c403b3ff74bdfcf06ff0df5841d
mando157 <mando.1572005@gmail.com>
2026-09-05 13:34:21 +0300

    complete Latest & Featured Sections with responsive

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,7 @@ function changeColor(colorName) {
 }
 
 // ^ Changing Image
-function changeImg(imgSrc , image , common , attribute) {
+function changeImg(imgSrc, image, common, attribute) {
     let srcOfImg = image.getAttribute(attribute),
         srcNameArr = srcOfImg.split("/");
 
@@ -17,4 +17,65 @@ function changeImg(imgSrc , image , common , attribute) {
 
     image.setAttribute(attribute, srcName);
 
+}
+
+// ^ Latest Section
+
+// * Create Li Element for Images
+function createLiImgEle(imagesArr) {
+    let LiElem = "";
+
+    imagesArr.forEach(function (imgName) {
+        LiElem += `
+            <li class="mainBorder rounded-2 p-2">
+                <img src="./images/products/${imgName}" class="img-fluid" alt="shoes" onclick="changePhoto('${imgName}' , this)">
+            </li>
+        `
+    });
+
+    return LiElem;
+}
+
+// * Create Li Element for Sizes
+function createLiSizeEle(sizeArr) {
+    let LiElem = "";
+
+    sizeArr.forEach(function (size) {
+        LiElem += `
+        <li class="mainButton mainBorder rounded-2 ${size === sizeArr[0] ? "active" : ""}" onclick="ChangeActive(this)">${size}</li>
+        `
+    });
+
+    return LiElem;
+}
+
+// ^ Featured Section
+function createLiEle(imageList) {
+    let indicator = "";
+
+    imageList.forEach(function (image) {
+        indicator += `
+            <li class="mainButton ${image == imageList[0] ? "active" : ""}" onclick="changePhoto('${image}' , this);ChangeActive(this)"></li>
+        `
+    });
+
+    return indicator;
+}
+
+// * Change Photo
+function changePhoto(imageName , that) {
+    let selectedImg = that.closest(".item").querySelector(".image img"),
+        srcImgArr = selectedImg.src.split("/");
+
+        srcImgArr[srcImgArr.length - 1] = imageName;
+
+        selectedImg.setAttribute("src" , srcImgArr.join("/"));
+}
+
+// * Change Indicator Active 
+function ChangeActive(that){
+    let currentEle = that.parentElement.querySelector(".active") ;
+
+    currentEle.classList.remove("active");
+    that.classList.add("active");
 }
--- js/script.js
+++ js/script.js
@@ -16,7 +16,15 @@ let
     loadingGif = document.querySelector(".loading"),
 
     navbar = document.querySelector("nav.navbar"),
-    navLinks = navbar.querySelectorAll(".nav-link");
+    navLinks = navbar.querySelectorAll(".nav-link"),
+
+    // ^ Latest Section variables
+    productsContainer = document.querySelector("#Latest .content"),
+
+    // ^ Featured Section variables
+    featuresContainer = document.querySelector("#Featured .content .row")
+
+    ;
 
 
 // ^ Loading Section
@@ -78,13 +86,108 @@ navLinks.forEach(function (link) {
         e.preventDefault();
 
         let currentNavLink = navbar.querySelector(".nav-link.active"),
-        currentId = link.getAttribute("href"),
-        currentSection = document.querySelector(currentId),
-        sectionTop = currentSection.offsetTop - navbar.clientHeight;
+            currentId = link.getAttribute("href"),
+            currentSection = document.querySelector(currentId),
+            sectionTop = currentSection.offsetTop - navbar.clientHeight;
 
         currentNavLink.classList.remove("active");
         link.classList.add("active");
 
-        window.scrollTo(0 , sectionTop);
+        window.scrollTo(0, sectionTop);
     });
+});
+
+window.addEventListener("scroll", function () {
+
+    navLinks.forEach(function (link) {
+
+        let currentId = link.getAttribute("href")
+        currentSection = document.querySelector(currentId),
+            sectionTop = currentSection.offsetTop - navbar.clientHeight,
+            sectionBottom = sectionTop + currentSection.clientHeight;
+
+        if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+
+            navbar.querySelector(".nav-link.active")?.classList.remove("active");
+
+            link.classList.add("active");
+        }
+
+    });
+
+});
+
+// ^ Latest Section
+latest.forEach(function (product) {
+
+    productsContainer.innerHTML += `
+        <div class="row mainBorder rounded-4 bg-white py-4 mx-4">
+            <div class="part-1 col-lg-6">
+                <div class="item row">
+                    <div class="photos col-12 col-lg-2 px-3">
+                        <ul class="d-flex flex-row flex-lg-column gap-2 p-0 m-0">
+                        ${createLiImgEle(product.images)}
+                        </ul>
+                    </div>
+                    <div class="image col-lg-10">
+                        <img src="./images/products/${product.images[0]}" class="img-fluid" alt="shoes">
+                    </div>
+                </div>
+            </div>
+
+            <div class="part-2 col-lg-6">
+                <h3 class="main-color">${product.name}</h3>
+                <p class="text-muted">${product.description}</p>
+
+                <p id="Price">
+                    <span class="fw-bolder me-2">Price :</span>
+                    <span class="main-color me-1 ${(product.discount == 0) ? "d-none" : ""}"> <del>${product.price}<sup>$</sup></del></span>
+                    <span>${product.price * (1 - product.discount)}<sup>$</sup></span>
+                </p>
+
+                <div id="Size" class="d-flex gap-1 align-items-center">
+                    <span class="fw-bolder me-2">Size :</span>
+                    <ul class="d-flex gap-2 p-0 m-0">
+                        ${createLiSizeEle(product.sizes)}
+                    </ul>
+                </div>
+
+                <button class="add-to-cart mainButton rounded-2 p-2">Add to Cart</button>
+            </div>
+        </div>
+    `
+});
+
+// ^ Featured Section
+features.forEach(function (product) {
+    featuresContainer.innerHTML += `
+        <div class="box col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="item bg-white px-4 py-3 rounded-3 text-center">
+                <p class="discount ${(product.discount == 0) ? 'd-none' : ""}" >-${product.discount * 100}%</p>
+
+                <div class="image">
+                    <img src="./images/products/${product.images[0]}" class="img-fluid" alt="product">
+                </div>
+
+                <div class="search d-flex justify-content-center align-items-center rounded-circle">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+
+                <ul class="d-flex justify-content-center align-items-center gap-2 list-unstyled mb-0">
+                    ${createLiEle(product.images)}
+                </ul>
+
+                <div class="text">
+                    <h6>${product.name}</h6>
+
+                    <p>
+                        <span class="main-color me-1 ${(product.discount == 0) ? 'd-none' : ""}">
+                            <del>${product.price}<sup>$</sup></del>
+                        </span>
+                        <span>${(product.price * (1 - product.discount)).toFixed(2)}<sup>$</sup></span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    `
 });
